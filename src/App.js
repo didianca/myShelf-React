104ef9241ad663c498519d4538fbbc8371038bb4
didianca <cazaranka@gmail.com>
2019-06-11 06:18:47 +1200

    set up table of products assigned proper bootstrap classes to make it pretty

--- src/App.js
+++ src/App.js
@@ -25,63 +25,86 @@ class App extends React.Component {
     addProduct = async _ => {
         const {product} = this.state;
         const headers = new Headers();
-        headers.append('Content-Type','application/json');
+        headers.append('Content-Type', 'application/json');
         const options = {
-            method:'POST',
+            method: 'POST',
             headers,
-            body:JSON.stringify(product)
+            body: JSON.stringify(product)
         };
-        const request = new Request('http://localhost:4000/products',options);
+        const request = new Request('http://localhost:4000/products', options);
         const response = await fetch(request);
         let status = await response.status;
         //if(product.price.isNaN || product.count.isNaN) return status = 400;
-        if(status === 200){
+        if (status === 200) {
             this.getProducts()
         }
     };
-    renderProduct = ({id, name}) => <div key={id}>{name}</div>;
+    renderProduct = ({id, name, price, count}) => <tr>
+        <td key={id}>{name}</td>
+        <td key={id}>{price} </td>
+        <td key={id}>{count}</td>
+    </tr>;
 
     render() {
         const {products, product} = this.state;
-        const {name,count,price} = product;
-        let classes = 'btn  btn-lg m-5 btn-';
-        classes += !(name && price && count ) ? "info" :'basic';
         return (
             <div className='App'>
                 <div>
                     <h1> LIST OF FRUITS</h1>
-                    <div className='badge badge-info m-2'>
-                        {products.map((this.renderProduct))}
+                    <div className='table-responsive'>
+                        <table className='table  table-bordered table-hover table-sm'>
+                            <caption>List of products</caption>
+                            <thead>
+                            <tr className='thead-dark'>
+                                <th>Product</th>
+                                <th>Price</th>
+                                <th>Count</th>
+                            </tr>
+                            </thead>
+                            <tbody className='table-info'>
+                            {products.map((this.renderProduct))}
+                            </tbody>
+
+                        </table>
                     </div>
                 </div>
                 <div>
                     <div>
                         <p>-name-</p>
                         <input
-                        value={product.name}
-                        onChange={e => this.setState({product: {...product, name: e.target.value}})}/>
+                            value={product.name}
+                            onChange={e => this.setState({product: {...product, name: e.target.value}})}/>
                     </div>
                     <div>
                         <p>-price-</p>
                         <input
-                        type='number'
-                        value={product.price}
-                        onChange={e => this.setState({product: {...product, price: e.target.value}})}
-                    />
+                            type='number'
+                            value={product.price}
+                            onChange={e => this.setState({product: {...product, price: e.target.value}})}
+                        />
                     </div>
                     <div>
                         <p>-count-</p>
                         <input
                             type='number'
-                        value={product.count}
-                        onChange={e => this.setState({product: {...product, count: e.target.value}})}
-                    />
+                            value={product.count}
+                            onChange={e => this.setState({product: {...product, count: e.target.value}})}
+                        />
                     </div>
-                    <button className={classes} onClick={this.addProduct}>Add Product</button>
+                    <button className={this.getBtnClasses()} onClick={this.addProduct}>Add Product</button>
                 </div>
             </div>
         )
     }
+
+    getBtnClasses() {
+        const {product} = this.state;
+        const {name, count, price} = product;
+        let classes = 'btn  btn-lg m-5 btn-';
+        classes += (!(name && price && count)) ? "basic" : 'info';
+        return classes
+    }
+
 }
 
 export default App;
