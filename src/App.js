3555ec63c318a32578627b5e4ef2a38cf4606037
didianca <cazaranka@gmail.com>
2019-06-11 08:51:50 +1200

    adjusted inputs and css

--- src/App.js
+++ src/App.js
@@ -12,10 +12,12 @@ class App extends React.Component {
             count: ''
         }
     };
+
     //no idea \/
     componentDidMount() {
         this.getProducts();
     }
+
     //get request to get all items in the db --populate table
     // use arrow function so the this keyword is inherited there for you have access to the parent class
     getProducts = () => {
@@ -47,72 +49,78 @@ class App extends React.Component {
         <td>{price} </td>
         <td>{count}</td>
     </tr>;
+
     //setting up btn classes for cleaner code in the render method
     getBtnClasses() {
         const {product} = this.state;
         const {name, count, price} = product;
         let classes = 'btn  btn-lg m-5 btn-';
-        classes += (!(name && price && count)) ? "basic" : 'info';
+        classes += (!(name && price && count)) ? "basic" : "info";
         return classes
     }
+
     //render final results
     render() {
-        const {products,product} = this.state;
+        const {products, product} = this.state;
         return (
             <div className='App'>
                 <h1> LIST OF FRUITS</h1>
                 <div className='table-responsive'>
-                        <table className='table  table-bordered table-hover table-sm'>
-                            <caption>List of products</caption>
-                            <thead>
-                            <tr className='thead-dark'>
-                                <th>Product</th>
-                                <th>Price</th>
-                                <th>Count</th>
-                            </tr>
-                            </thead>
-                            <tbody className='table-info'>
-                            {this.state.products.length === 0 && <tr>
-                                <td> - </td>
-                                <td> - </td>
-                                <td> - </td>
-                            </tr>}
-                            {products.map((this.renderProduct))}
-                            </tbody>
+                    <table className='table  table-bordered table-hover table-sm'>
+                        <caption>List of products</caption>
+                        <thead>
+                        <tr className='thead-dark'>
+                            <th>Product</th>
+                            <th>Price</th>
+                            <th>Count</th>
+                        </tr>
+                        </thead>
+                        <tbody className='table-info'>
+                        {this.state.products.length === 0 && <tr>
+                            <td> -</td>
+                            <td> -</td>
+                            <td> -</td>
+                        </tr>}
+                        {products.map((this.renderProduct))}
+                        </tbody>
 
-                        </table>
+                    </table>
                 </div>
-                <div>
-                    <div>
-                        <p>-name-</p>
+                <div >
+                    <div className='border border-info'>
                         <input
+                            className='form-control'
+                            placeholder='name'
                             value={product.name}
                             onChange={e => this.setState({product: {...product, name: e.target.value}})}/>
                     </div>
-                    <div>
-                        <p>-price-</p>
+                    <div className='border border-info'>
                         <input
+                            className='form-control'
+                            placeholder='price'
                             type='number'
                             value={product.price}
                             onChange={e => this.setState({product: {...product, price: e.target.value}})}/>
                     </div>
-                    <div>
-                        <p>-count-</p>
+                    <div className='border border-info'>
                         <input
+                            className='form-control'
+                            placeholder='count'
                             type='number'
                             value={product.count}
                             onChange={e => this.setState({product: {...product, count: e.target.value}})}/>
                     </div>
+
                 </div>
                 <button
                     className={this.getBtnClasses()}
                     onClick={this.addProduct}>
                     Add Product
-                </button></div>
+                </button>
+            </div>
 
         )
     }
-
 
 
 }
